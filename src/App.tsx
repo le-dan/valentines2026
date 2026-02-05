2c332c6048976c2242faafbbb52ecef062d89bdf
mineult <tle57@gmu.edu>
2026-02-05 00:40:44 -0500

    add questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,57 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import ProgressBar from "./components/ProgressBar";
+import { motion, useInView } from "motion/react";
 
 function App() {
+	const ref = useRef(null);
+	const isInView = useInView(ref, { once: true });
+	const question = "What type of cat is this called?";
+
+	const [showImage, setShowImage] = useState(false);
+	const [showButtons, setShowButtons] = useState(false);
+
+	const [progress, setProgress] = useState(0);
 	return (
 		<>
-			<ProgressBar />
+			<ProgressBar progress={progress} />
+			<div ref={ref} className="text-center mt-15 text-(--text-primary) text-3xl font-extralight">
+				{question.split("").map((letter, index) => (
+					<motion.span
+						key={index}
+						initial={{ opacity: 0 }}
+						animate={isInView ? { opacity: 1 } : {}}
+						transition={{ duration: 0.25, delay: index * 0.05 }}
+						onAnimationComplete={index === question.length - 1 ? () => setShowImage(true) : undefined}
+					>
+						{letter}
+					</motion.span>
+				))}
+			</div>
+			{showImage && (
+				<motion.img
+					src="./src/assets/munchkincat.jpg"
+					alt="A cute cat"
+					className="w-3/4 mx-auto my-10 rounded-lg"
+					initial={{ opacity: 0, y: 50 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{ duration: 0.5 }}
+					onAnimationComplete={() => setShowButtons(true)}
+				/>
+			)}
+			{showButtons && (
+				<motion.div
+					className="grid grid-cols-2 grid-rows-2 justify-center gap-4 mt-10 h-1/2"
+					initial={{ opacity: 0, y: 20 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{ duration: 0.5 }}
+				>
+					<button className="bg-(--primary) h-10 text-white px-6 py-2 rounded-md hover:bg-(--primary) transition">Munchkin</button>
+					<button className="bg-(--primary) h-10 text-white px-6 py-2 rounded-md hover:bg-(--primary) transition">Siamese</button>
+					<button className="bg-(--primary) h-10 text-white px-6 py-2 rounded-md hover:bg-(--primary) transition">Persian</button>
+					<button className="bg-(--primary) h-10 text-white px-6 py-2 rounded-md hover:bg-(--primary) transition">Tabby</button>
+				</motion.div>
+			)}
 		</>
 	);
 }
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
-
-export default function ProgressBar() {
-	const [progress, setProgress] = useState(50);
+export default function ProgressBar(props: { progress: number }) {
 	return (
 		<div className="w-full h-2 bg-gray-200 rounded-3xl">
-			<div className="progress bg-(--primary) h-full rounded-3xl" style={{ width: progress + "%" }}></div>
+			<div className="progress bg-(--primary) h-full rounded-3xl" style={{ width: props.progress + "%", boxShadow: "0 0 15px 2px pink" }}></div>
 		</div>
 	);
 }
